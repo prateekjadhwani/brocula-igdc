5079476f5633066a46f8d00130d9477e58522569
Prateek Jadhwani <prateekjadhwani@users.noreply.github.com>
2022-10-26 20:29:41 +0530

    WakeLock

--- base.js
+++ base.js
@@ -2,15 +2,40 @@
     const broculaIGDC = () => {
         let type = 0;
         let intervalObj;
+        let wakeLock = null;
 
         const init = () => {
             startSlideShow();
             handleFullScreen();
             
+
             const imageContainer = document.querySelector('.brocula-hi-container');
             imageContainer.addEventListener('click', () => {
                 handleImageClick();
-            })
+            });
+
+            document.addEventListener('visibilitychange', () => {
+                handleScreenLock();
+            });
+            
+        };
+
+        const requestWakeLock = async () => {
+            try {
+              wakeLock = await navigator.wakeLock.request();
+              wakeLock.addEventListener('release', () => {
+                console.log('Screen Wake Lock released:', wakeLock.released);
+              });
+              console.log('Screen Wake Lock released:', wakeLock.released);
+            } catch (err) {
+              console.error(`${err.name}, ${err.message}`);
+            }
+        };
+
+        const handleScreenLock = async () => {
+            if (wakeLock !== null && document.visibilityState === 'visible') {
+              await requestWakeLock();
+            }
         };
 
         const handleFullScreen = () => {
